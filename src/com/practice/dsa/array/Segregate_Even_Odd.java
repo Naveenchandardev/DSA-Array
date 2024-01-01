fd32c6175bd9e30f57720f56e255330f92018970
Naveenchandardev <chandarnaveen88@gmail.com>
2024-01-01 13:57:51 +0530

    #segregate odd and even

--- src/com/practice/dsa/array/Segregate_Even_Odd.java
+++ src/com/practice/dsa/array/Segregate_Even_Odd.java
@@ -1,10 +1,26 @@
 package com.practice.dsa.array;
 
-public class Segregate_Even_Odd {
+import java.util.Arrays;
 
+public class Segregate_Even_Odd {
 	public static void main(String[] args) {
 		int [] a = {2,5,4,8,9,6,3,5,7};
- 
+		myMethod(a, a.length);
 	}
 
+	// METHOD 1:
+	private static void myMethod(int[] a, int length) {
+		int [] c = a.clone();
+		int finalkey =a.length-1; int m=0;
+		for(int i=0;i<a.length;i++) {
+			if(a[i]%2==0) {
+				c[m]=a[i];m++;
+			}
+			else {
+				c[finalkey]=a[i];
+				finalkey --;
+			}
+		}
+		System.out.println(Arrays.toString(c));		
+	}
 }
