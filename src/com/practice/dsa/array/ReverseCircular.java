42a1dce32f64a0b8d43bdf595de0b1cd2a790f46
Naveenchandardev <chandarnaveen88@gmail.com>
2024-02-24 18:53:39 +0530

    #cirucular array

--- src/com/practice/dsa/array/ReverseCircular.java
+++ src/com/practice/dsa/array/ReverseCircular.java
@@ -1,9 +1,6 @@
 package com.practice.dsa.array;
-
 import java.util.Arrays;
-
 public class ReverseCircular {
-
 	public static void main(String[] args) {
 		int [] a = {2,5,8,9,6,5,4,1,2};
 		int n= a.length;
@@ -11,15 +8,15 @@ public class ReverseCircular {
 		int position = 4;
 		for(int i=0;i<n;i++) {
 			if(position<n ) {
-				b[i]= a[position];position++;
+				b[i]= a[position];
+				position++;
 			}
 			else if(position==a.length) {
-				position=0;b[i]= a[position];position++;
-				
+				position=0;
+				b[i]= a[position];
+				position++;	
 			}
 		}
 		System.out.println(Arrays.toString(b));
-
 	}
-
 }
