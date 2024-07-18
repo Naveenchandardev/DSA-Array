99e4e9ba88204186865020d88b9fa9ac3c607b78
Naveenchandardev <chandarnaveen88@gmail.com>
2024-07-18 22:29:43 +0530

    #leetcode; pass pillow ; and find person;

--- src/com/practice/dsa/array/leetcode/PassPillow.java
+++ src/com/practice/dsa/array/leetcode/PassPillow.java
@@ -3,8 +3,15 @@ package com.practice.dsa.array.leetcode;
 public class PassPillow {
     //https://leetcode.com/problems/pass-the-pillow/description/?envType=daily-question&envId=2024-07-06
     public static void main(String[] args) {
-        int persons=4;
-        int time=5;
+        int persons=18;
+        int time=38;
+        int res1 =soulution1(persons,time);
+        int res2 =soulution2(persons,time);
+        System.out.println(res1);
+        System.out.println(res2);
+    }
+
+    private static int soulution2(int persons, int time) {
         int res =0;
         boolean ch=false;
         for(int i=0;i<=time;i++){
@@ -16,6 +23,12 @@ public class PassPillow {
                 ch=true;
             }
         }
-        System.out.println(res);
+        return res;
+    }
+
+    private static int soulution1(int n, int time) {
+        int chunks = time / (n - 1);
+        return chunks % 2 == 0 ? (time % (n - 1) + 1) : (n - time % (n - 1));
+
     }
 }
