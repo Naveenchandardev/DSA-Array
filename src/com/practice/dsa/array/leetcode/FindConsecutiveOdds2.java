108a782d1e43ba2be3af2053483f44482b254299
Naveenchandardev <chandarnaveen88@gmail.com>
2024-07-19 22:30:03 +0530

    #leetcode; find 3 consecutive odds;;

--- src/com/practice/dsa/array/leetcode/FindConsecutiveOdds2.java
+++ src/com/practice/dsa/array/leetcode/FindConsecutiveOdds2.java
@@ -3,8 +3,9 @@ package com.practice.dsa.array.leetcode;
 public class FindConsecutiveOdds2 {
     //https://leetcode.com/problems/three-consecutive-odds/description/?envType=daily-question&envId=2024-07-01
     public static void main(String[] args) {
-        int []a={2,6,4,1};
+        int []a={2,6,4,3,3,1};
         boolean result = solution(a);
+        System.out.println(result);
     }
     public static boolean solution(int[] arr) {
         int count = 0;
