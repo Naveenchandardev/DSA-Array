d32119d8eea70985d0cedd159bc5764f1982e30c
Naveenchandar <chandarnaveen88@gmail.com>
2025-02-23 10:24:09 +0530

    #interviewQuestions: 2nd largest element

--- src/com/practice/dsa/array/leetcodeTwo/Stocks_2.java
+++ src/com/practice/dsa/array/leetcodeTwo/Stocks_2.java
@@ -2,18 +2,17 @@ package com.practice.dsa.array.leetcodeTwo;
 
 public class Stocks_2 {
     public static void main(String[] args) {
-        int[] prices = {7, 1, 5, 3, 6, 4};
+        int[] prices = {2,1};
         System.out.println("Max Profit: " + maxProfit(prices)); // Output: 5
     }
     private static int maxProfit(int[] prices) {
-        int minPrice = Integer.MAX_VALUE;  // Store the minimum price seen so far
-        int maxProfit = 0;                 // Track max profit
-
+        int minPrice = Integer.MAX_VALUE;
+        int maxProfit = 0;
         for (int price : prices) {
             if (price < minPrice) {
-                minPrice = price;  // Update min price
+                minPrice = price;
             } else {
-                maxProfit = Math.max(maxProfit, price - minPrice);  // Calculate profit
+                maxProfit = Math.max(maxProfit, price - minPrice);
             }
         }
         return maxProfit;
