d71632f47f40e3317d5c7f97e5661c4459e4f52c
Naveenchandardev <chandarnaveen88@gmail.com>
2026-07-02 20:00:07 +0530

    #merge 2 array

--- src/com/practice/dsa/array/Rotate_Array.java
+++ src/com/practice/dsa/array/Rotate_Array.java
@@ -4,12 +4,13 @@ import java.util.Arrays;
 
 public class Rotate_Array {
     public static void main(String[] args) {
-int [] a= {1,2,3,4,5};int[]b=new int[a.length];
-int in=2;
-        for(int i=0;i<a.length;i++){
-        if(in<a.length)b[i]=a[in++];
-        else b[i]=a[in++-5];
+        int[] a = {1, 2, 3, 4, 5};
+        int[] b = new int[a.length];
+        int in = 2;
+        for (int i = 0; i < a.length; i++) {
+            if (in < a.length) b[i] = a[in++];
+            else b[i] = a[in++ - 5];
         }
         System.out.println(Arrays.toString(b));
-        }
+    }
 }
---
+++ src/com/practice/dsa/array/mergeTwoSortedArrays.java
@@ -0,0 +1,34 @@
+package com.practice.dsa.array;
+
+import java.util.Arrays;
+
+public class mergeTwoSortedArrays {
+    public static void main(String[] args) {
+        int[] a = {1,3,5};
+        int[] b = {2,4,6};
+        int[] result = new int[a.length + b.length];
+
+        int i = 0;
+        int j = 0;
+        int k = 0;
+
+        while (i < a.length && j < b.length) {
+                   if (a[i] <= b[j]) {
+                result[k++] = a[i++];
+            } else {
+                result[k++] = b[j++];
+            }
+        }
+
+        while (i < a.length) {
+            result[k++] = a[i++];
+        }
+
+        while (j < b.length) {
+            result[k++] = b[j++];
+        }
+
+        System.out.println(Arrays.toString(result));
+
+    }
+}
